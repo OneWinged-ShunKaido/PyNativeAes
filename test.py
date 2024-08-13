d3f9e15790644151b1070d80a73884e2d9755f8b
OneWinged-ShunKaido <47723046+OneWinged-ShunKaido@users.noreply.github.com>
2024-08-14 01:50:10 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+from secrets import token_bytes
+from PyNativeAes import NativeAes
+#
+
+message = b'Hello World'
+iv = b'\x00' * 16  # weak iv
+key = token_bytes(16)  #  usage of secrets lib not recommend at all !!
+with NativeAes() as native_aes:
+  self.iv = iv
+  self.key = key
+  encrypted_message = native_aes.encrypt(message)
+  decrypted_message = native_aes.decrypt(encrypted_message)
+
+print(f'Encrypted -> {encrypted_message}')
+print('Decrypted -> {decrypted_message.decode()}')
